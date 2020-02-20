5722d679fe77846fa7f8fdd1f3e95d621b7c99a3
ampcpmgp <ampcpmgp@gmail.com>
2020-02-21 00:40:03 +0900

    Update mock-config.js

--- mock/mock-config.js
+++ mock/mock-config.js
@@ -1,11 +1,9 @@
 import mock from 'am-mocktimes'
 import { replace } from 'svelte-spa-router'
 import sleep from '../src/utils/sleep'
-import * as greetings from '../src/states/greetings'
 
 mock({
   sleep,
-  greetings,
 
   page(name) {
     replace(name)
