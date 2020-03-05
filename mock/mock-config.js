1b5f40a942e43e575d45cb647d02032c67aa9ee2
ampcpmgp <ampcpmgp@gmail.com>
2020-03-05 23:01:56 +0900

    モック環境構築 (#8)
    * モック環境の作成
    
    * remove console.log
    
    * Update README.md

--- mock/mock-config.js
+++ mock/mock-config.js
@@ -1,6 +1,9 @@
 import mock from 'am-mocktimes'
+import fetchMock from 'fetch-mock'
 import { replace } from 'svelte-spa-router'
 import sleep from '../src/utils/sleep'
+
+fetchMock.mock('/api/users/account/get', {})
 
 mock({
   sleep,
