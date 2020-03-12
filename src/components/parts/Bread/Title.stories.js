70e6d955505dd62e043d3351d0e2c5f57bc8c52f
ampcpmgp <ampcpmgp@gmail.com>
2020-03-12 14:23:32 +0900

    パン画面タイトル作成

---
+++ src/components/parts/Bread/Title.stories.js
@@ -0,0 +1,34 @@
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+import Title from './Title.svelte'
+
+export default {
+  title: 'Bread Title',
+  component: Title,
+  decorators: [withKnobs],
+}
+
+export const 編集可能 = () => ({
+  Component: Title,
+  props: {
+    name: text('name', ''),
+    userId: text('userId', 'dummy-id__0123456789'),
+    readonly: boolean('readonly', false),
+    errMsg: text('errMsg', 'タイトルは30文字以内で入力してください。'),
+  },
+  on: {
+    input: console.info,
+  },
+})
+
+export const 編集不可 = () => ({
+  Component: Title,
+  props: {
+    name: text(
+      'name',
+      '最大文字数＿３０文字最大文字数＿３０文字最大文字数＿３０文字'
+    ),
+    userId: text('userId', 'dummy-id__0123456789'),
+    readonly: boolean('readonly', true),
+    errMsg: text('errMsg', ''),
+  },
+})
