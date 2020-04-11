cd71624d45ec605cb0aaf3f07cab213348fedb64
ampcpmgp <ampcpmgp@gmail.com>
2020-04-11 15:50:16 +0900

    WIP

--- api/user/bread/get.js
+++ api/user/bread/get.js
@@ -11,6 +11,8 @@ module.exports = handleApiError(async (req, res) => {
       q.Get(q.Match(q.Index('breads_by_nano_id'), nanoId))
     )
 
+    // TODO: ユーザー不一致ならisPublicの場合返さない。
+
     res.json(response.data)
   } catch (error) {
     if (error.name === 'NotFound') {
