3530128671086d857d6c4d39c076f6b4a45510b2
ampcpmgp <ampcpmgp@gmail.com>
2020-04-12 23:50:34 +0900

    キャッシュ欄に説明追加

--- src/components/containers/BreadGroup/Cache.svelte
+++ src/components/containers/BreadGroup/Cache.svelte
@@ -5,5 +5,5 @@
 
 <Group
   type={BreadType.CACHE}
-  description="IndexedDB
+  description="工事中<br><br>パン詳細画面を開くと、IndexedDB
   を通してブラウザに保存されます。共有端末をご利用の方で、閲覧済みキャッシュを見られて困る方は、プライベートウィンドウでご利用ください。" />
