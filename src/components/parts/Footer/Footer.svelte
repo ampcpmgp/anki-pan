f8335fb287962a61de12566c011406e59d11eb80
ampcpmgp <ampcpmgp@gmail.com>
2020-04-14 23:01:52 +0900

    フッター作成

--- src/components/parts/Footer/Footer.svelte
+++ src/components/parts/Footer/Footer.svelte
@@ -1,9 +1,28 @@
+<script>
+  import { default as Octocat } from '../../../images/Octocat.jpg'
+  import { default as Twitter } from '../../../images/Twitter_Logo_Blue.svg'
+</script>
+
 <style>
   footer {
     display: grid;
-    border-top: solid 1px dimgray;
+    grid-auto-flow: column;
+    justify-content: start;
+    border-top: 1px solid black;
     padding: 8px;
+  }
+
+  img {
+    width: auto;
+    height: 60px;
   }
 </style>
 
-<footer>フッター欄</footer>
+<footer>
+  <a href="https://github.com/ampcpmgp/anki-pan/" target="_blank">
+    <img src={Octocat} alt="" />
+  </a>
+  <a href="https://twitter.com/anki_pan_dev" target="_blank">
+    <img src={Twitter} alt="" />
+  </a>
+</footer>
