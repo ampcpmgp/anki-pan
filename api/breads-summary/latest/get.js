9e215caf071f64c960e0588059932091d8303efa
ampcpmgp <ampcpmgp@gmail.com>
2020-04-17 23:19:50 +0900

    最新取得個数を10->5に変更

--- api/breads-summary/latest/get.js
+++ api/breads-summary/latest/get.js
@@ -2,12 +2,12 @@ const { q, client } = require('../../_utils/faunadb')
 const { handleApiError } = require('../../_utils/api-error')
 
 module.exports = handleApiError(async (req, res) => {
-  // 最新10件取得
+  // 最新5件取得
   if (!req.query.ref || !req.query.ts) {
     const response = await client.query(
       q.Map(
         q.Paginate(q.Match(q.Index('breads_sort_by_ts_desc'), true), {
-          size: 10,
+          size: 5,
         }),
         q.Lambda(['ts', 'ref'], {
           nanoId: q.Select(['data', 'nanoId'], q.Get(q.Var('ref'))),
@@ -21,11 +21,11 @@ module.exports = handleApiError(async (req, res) => {
     return
   }
 
-  // 特定のパンから後ろに最新10件取得
+  // 特定のパンから後ろに最新5件取得
   const response = await client.query(
     q.Map(
       q.Paginate(q.Match(q.Index('breads_sort_by_ts_desc'), true), {
-        size: 10,
+        size: 5,
         after: [req.query.ts - 0, q.Ref(q.Collection('Breads'), req.query.ref)],
       }),
       q.Lambda(['ts', 'ref'], {
