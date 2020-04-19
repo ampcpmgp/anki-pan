8293564d5a33cb21aabcc91cc43eaab87ed59b3c
ampcpmgp <ampcpmgp@gmail.com>
2020-04-19 12:12:50 +0900

    API作成

--- api/user/favorite/get.js
+++ api/user/favorite/get.js
@@ -1,12 +1,22 @@
-module.exports = (req, res) => {
+const { getUserInfo } = require('../../_utils/auth0')
+const { handleApiError } = require('../../_utils/api-error')
+const { getDBUser, client, q } = require('../../_utils/faunadb')
+
+module.exports = handleApiError(async (req, res) => {
+  const response = await getUserInfo(req)
+
+  const { sub: subjectClaim } = await response.json()
+  const { nanoId: userNanoId } = await getDBUser(subjectClaim)
+
   const {
-    query: { breadId },
+    query: { breadNanoId },
   } = req
 
-  const bread = {}
+  const isExists = await client.query(
+    q.Exists(q.Match(q.Index('favorites_by_ids'), [userNanoId, breadNanoId]))
+  )
 
   res.json({
-    breadId,
-    bread,
+    isExists,
   })
-}
+})
