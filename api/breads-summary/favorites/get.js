dd5a728016e8d20d2331fc7ce2c5917ec898c58d
ampcpmgp <ampcpmgp@gmail.com>
2020-04-23 13:15:52 +0900

    お気に入り取得機能実装

--- api/breads-summary/favorites/get.js
+++ api/breads-summary/favorites/get.js
@@ -1,23 +1,56 @@
-const { q, client } = require('../../../_utils/faunadb')
-const { handleApiError, ApiError } = require('../../../_utils/api-error')
+const { q, client, getDBUser } = require('../../_utils/faunadb')
+const { getUserInfo } = require('../../_utils/auth0')
+const { handleApiError } = require('../../_utils/api-error')
 
-// 未実装
 module.exports = handleApiError(async (req, res) => {
-  if (!req.query.ref || !req.query.ts) {
-    throw new ApiError('Validation Error', 400)
+  const responseUserInfo = await getUserInfo(req)
+
+  const { sub: subjectClaim } = await responseUserInfo.json()
+  const user = await getDBUser(subjectClaim)
+
+  // 最新5件取得
+  if (!req.query.ref || !req.query.nanoId || !req.query.ts) {
+    const response = await client.query(
+      q.Map(
+        q.Map(
+          q.Paginate(
+            q.Match(q.Index('favorites_by_user_nano_id'), user.nanoId),
+            {
+              size: 5,
+            }
+          ),
+          q.Lambda(
+            ['ts', 'breadNanoId'],
+            q.Get(q.Match(q.Index('breads_by_nano_id'), q.Var('breadNanoId')))
+          )
+        ),
+        q.Lambda('item', {
+          nanoId: q.Select(['data', 'nanoId'], q.Var('item')),
+          title: q.Select(['data', 'title'], q.Var('item')),
+          userId: q.Select(['data', 'userId'], q.Var('item')),
+        })
+      )
+    )
+    res.json(response)
+    return
   }
 
+  // 特定のパンから後ろに最新5件取得
   const response = await client.query(
     q.Map(
       q.Map(
-        q.Paginate(q.Match(q.Index('breads_sort_by_ts_desc'), true), {
+        q.Paginate(q.Match(q.Index('favorites_by_user_nano_id'), user.nanoId), {
           size: 5,
           after: [
             req.query.ts - 0,
+            req.query.nanoId,
             q.Ref(q.Collection('Breads'), req.query.ref),
           ],
         }),
-        q.Lambda(['ts', 'ref'], q.Get(q.Var('ref')))
+        q.Lambda(
+          ['ts', 'breadNanoId'],
+          q.Get(q.Match(q.Index('breads_by_nano_id'), q.Var('breadNanoId')))
+        )
       ),
       q.Lambda('item', {
         nanoId: q.Select(['data', 'nanoId'], q.Var('item')),
--- src/components/containers/BreadGroup/Favorites.svelte
+++ src/components/containers/BreadGroup/Favorites.svelte
@@ -1,13 +1,30 @@
 <script>
   import BreadType from '../../../const/bread-type'
+  import {
+    fetch,
+    fetchReadMore,
+    existsAfter,
+    items,
+  } from '../../../states/breads-summary/favorites'
+  import { moveBreadDetail } from '../../../utils/router'
   import Group from '../../parts/Bread/Group'
+
+  let isReading = false
+
+  fetch()
+
+  async function onReadMore() {
+    isReading = true
+    await fetchReadMore()
+    isReading = false
+  }
 </script>
 
 <Group
   type={BreadType.FAVORITES}
-  items={[]}
-  description="工事中"
-  showReadMore={false}
-  isReading={false}
-  on:click={console.info}
-  on:readMore={console.info} />
+  items={$items}
+  showReadMore={$existsAfter}
+  description=""
+  {isReading}
+  on:click={moveBreadDetail}
+  on:readMore={onReadMore} />
