7bd334c99a3e5b0676c87d2a86dd4aaff95ef0d4
ampcpmgp <ampcpmgp@gmail.com>
2020-05-06 12:26:43 +0900

    半角文字数は60文字までOK

--- utils/validator/bread/title.js
+++ utils/validator/bread/title.js
@@ -1,5 +1,7 @@
 const Validation = require('../../../const/validation')
-const MAX_VALUE = 30
+const MAX_VALUE = 60
+const MAX_ZENKAKU_VALUE = 30
+const hasZenkaku = /[^\x20-\x7E\xA1-\xDF]+/
 
 exports.validate = value => {
   if (value.length === 0) {
@@ -8,6 +10,10 @@ exports.validate = value => {
 
   if (value.length > MAX_VALUE) {
     return Validation.COUNT_OVER
+  }
+
+  if (hasZenkaku.test(value) && value.length > MAX_ZENKAKU_VALUE) {
+    return Validation.COUNT_ZENKAKU_OVER
   }
 
   return Validation.NO_ERROR
@@ -25,7 +31,9 @@ exports.getErrMsg = value => {
     case Validation.EMPTY:
       return ''
     case Validation.COUNT_OVER:
-      return `最大文字数は${MAX_VALUE}文字です`
+      return `最大文字数は${MAX_ZENKAKU_VALUE}文字です`
+    case Validation.COUNT_ZENKAKU_OVER:
+      return `全角文字を含む場合、最大文字数は${MAX_ZENKAKU_VALUE}文字です`
     default:
       console.error(`result: ${result}`)
   }
